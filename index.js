d349319d688a807729af3b52eff52b78b94d21df
daniellopez821 <daniellopmor19@gmail.com>
2025-03-12 02:13:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-import mysql from 'mysql2'
-
 const express = require('express');
-const port = process.env.PORT || 3360;
-
+const multer = require('multer')
+const cors = require('cors');
 const app = express();
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Felipe02102*',
-    database: 'edify_website'
-}).promise()
+const port = 4200;
 
-//API MiddleWare
+app.use(cors());
 
-app.use(express.json()); // this is to accept data in json format
-
-app.use(express.urlencoded()); //Used to decode data send through html form
-
-//API Routes
-app.get('/add',(req,res)=>{
-
-    res.sendFile(_dirname + '/edify/src/app/cart-page/cart-page.component.html');
-})
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
-app.post('/add',(req,res)=>{
-    console.log(req.body); // the data we get is in the body of the request
-})
+const upload = multer({dest: 'files/'})
 
-
-app.listen(port,()=>{
-    console.log(`Server started at http://localhost:${port}`)
+app.get('/cart', function(req, res){
+    res.end('Welcome to api');
 });
+
+app.post('/cart', upload.any(),function (req, res){
+    
+    console.log(req.body);
+    console.log(req.files);
+});
+
+app.listen(4200);
+
+
 
