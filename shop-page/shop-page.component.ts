691ff081c494cc7da1003b3cfb499282b7885216
daniellopez821 <daniellopmor19@gmail.com>
2025-04-08 21:56:24 -0400

    Add files via upload

--- shop-page/shop-page.component.ts
+++ shop-page/shop-page.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-shop-page',
   imports: [],
   templateUrl: './shop-page.component.html',
+  standalone: true,
   styleUrl: './shop-page.component.css'
 })
-export class ShopPageComponent {
+export class ShopPageComponent{
 
 }
